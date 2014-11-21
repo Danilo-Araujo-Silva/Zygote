e696910fb051058e2655e7e48a54878c29e0caa5
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2014-11-21 13:52:12 -0200

    Fist commit.

---
+++ src/com/garoudan/zygote/model/vendor/android/app/Activity.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.garoudan.zygote.model.vendor.android.app;
+
+/**
+ * @author s15603
+ *
+ */
+public class Activity extends android.app.Activity {
+
+	/**
+	 * 
+	 */
+	public Activity() {
+	}
+}
