813557b87e734344b6d3d98cbefa04730d438f50
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2014-12-10 14:40:34 -0200

    Refactoring.

---
+++ src/com/garoudan/zygote/model/vendor/android/os/AsyncTask.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.garoudan.zygote.model.vendor.android.os;
+
+/**
+ * 
+ */
+abstract public class AsyncTask<Params, Progress, Result> extends android.os.AsyncTask<Params, Progress, Result> {
+
+	/**
+	 * 
+	 */
+	public AsyncTask() {
+	}
+}
